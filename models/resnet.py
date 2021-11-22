0149a183439d8cad8c4ce4edc94ce5990c9500cb
Lavanya Nemani <GIT_USERNAME+@gmail.com>
2021-11-22 16:29:23 +0000

    Update models/resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -9,14 +9,14 @@ class BasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-  
+
         if stride == 2:
           self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=2, bias=False)
         else:
           self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        
+
         self.bn1 = nn.BatchNorm2d(planes)
 
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
@@ -42,18 +42,18 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        self.in_planes = 8
         self.gradients = None
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3,
                                stride=1, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-        
+        self.bn1 = nn.BatchNorm2d(8)
+        self.layer1 = self._make_layer(block, 8, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 128, num_blocks[3], stride=2)
+        self.linear = nn.Linear(128*block.expansion, num_classes)
+
     def activations_hook(self, grad):
       self.gradients = grad
 
@@ -71,7 +71,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        
+
         if out.requires_grad:
             h = out.register_hook(self.activations_hook)
 
@@ -82,13 +82,13 @@ class ResNet(nn.Module):
 
     def get_activations_gradient(self):
       return(self.gradients)
-      
+
     def get_activations(self, x):
       return(self.layer4(self.layer3(self.layer2(self.layer1(F.relu(self.bn1(self.conv1(x))))))))
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(BasicBlock, [1, 1, 1, 1])
 
 
 def ResNet34():
