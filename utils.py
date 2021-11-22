57ea356b2823345dc4aabf60ab811cea7e42b2c1
Lavanya Nemani <GIT_USERNAME+@gmail.com>
2021-11-22 21:36:12 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -121,10 +121,10 @@ def plot_loss_accuracy(results):
     axs[0, 1].plot(test_losses)
     axs[0, 1].set_title("Test Loss")
 
-    axs[1, 0].plot(train_accuracy)
+    axs[1, 0].plot(train_acc)
     axs[1, 0].set_title("Training Accuracy")
 
-    axs[1, 1].plot(test_accuracy)
+    axs[1, 1].plot(test_acc)
     axs[1, 1].set_title("Test Accuracy")
     plt.show()
 
